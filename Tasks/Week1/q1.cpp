6f99e008324349593f502e77435dbe4bbe90a53b
Suresh-Krishna27 <155833887+Suresh-Krishna27@users.noreply.github.com>
2024-12-31 18:24:49 +0530

    Create q1.cpp

---
+++ Tasks/Week1/q1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int Odd(int ar[], int n)
+{
+    int res = 0; 
+    for (int i = 0; i < n; i++)     
+        res = res ^ ar[i];
+    
+    return res;
+}
+
+int main()
+{
+    int ar[] = {7, 2, 6, 4, 5, 2, 4, 6, 5, 2, 4, 4, 2};
+    int n = sizeof(ar)/sizeof(ar[0]);
+    cout << Odd(ar, n);
+    return 0;
+}
